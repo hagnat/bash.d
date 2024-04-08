f862b04370007a2d3b40cf053415cc726a4c71b9
Valdir Bruxel Junior <valdir.bruxel@cloudbeds.com>
2024-04-08 09:49:37 -0300

    shorten got script from 30s to 15s

--- shared/scripts/got.sh
+++ shared/scripts/got.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$( realpath ${SCRIPT_DIR}/../.. )
 
 echo -e "nah, na ... NAH NA nah, NAH NAH\n"
-afplay -t 30 ${ROOT_DIR}/var/assets/got-opening.mp3 &
+afplay -t 15 ${ROOT_DIR}/var/assets/got-opening.mp3 &
 
 CORRECT_COMMAND="git ${@}"
 echo "\$ ${CORRECT_COMMAND}"
